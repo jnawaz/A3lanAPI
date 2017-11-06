9ba43489a7e5720dc20b8d099a92592a07c5cbaa
Jamil Nawaz <jamilnawaz88@gmail.com>
2017-11-06 12:48:12 +0000

    Setting up express router.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.set('port', (process.env.PORT || 5000));
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+// test route to make sure everything is working (accessed at GET http://localhost:5000/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+app.use('/api', router);
 
 app.use(express.static(__dirname + '/public'));
 
