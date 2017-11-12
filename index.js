de9a3663d92a409c97fc32f46a660b84b0cc0faa
Jamil Nawaz <jamilnawaz88@gmail.com>
2017-11-12 09:15:44 +0000

    Index page

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
+  // response.render('pages/index');
   response.render('pages/index');
 });
 
