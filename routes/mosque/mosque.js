a53cd059888e011fc67b41b981b2c845a8449bae
Jamil Nawaz <jamilnawaz88@gmail.com>
2017-12-03 17:01:41 +0000

    Updated response messages for US010 and MO005

--- routes/mosque/mosque.js
+++ routes/mosque/mosque.js
@@ -79,7 +79,9 @@ router.get('/mosqueById', authMiddleware.authentication, function (req, res) {
             });
         } catch (e) {
             res.json(400).json({
-                
+                success: false, 
+                message: apiResponse.MO005,
+                code: 'MO005'
             });
         }
     });
